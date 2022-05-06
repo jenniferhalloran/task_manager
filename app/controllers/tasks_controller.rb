e3fa7f1fcce7478a2c8ae931eac7b9d8cfb5809f
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-06 15:48:16 -0400

    Completely forgot to commit anything as i went along..gah

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    # binding.pry
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    # binding.pry
+    task.save
+    # binding.pry
+    redirect_to '/tasks'
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to '/tasks'
+  end
+end
